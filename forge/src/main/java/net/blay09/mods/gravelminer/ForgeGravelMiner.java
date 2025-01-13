2c2ea46c324170e3e55d07d3ff6c08054f9fc2ea
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-01-13 20:40:34 +0100

    feat: Update for Forge 1.21.4

--- forge/src/main/java/net/blay09/mods/gravelminer/ForgeGravelMiner.java
+++ forge/src/main/java/net/blay09/mods/gravelminer/ForgeGravelMiner.java
@@ -3,16 +3,22 @@ package net.blay09.mods.gravelminer;
 import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.client.BalmClient;
+import net.blay09.mods.balm.forge.ForgeLoadContext;
 import net.blay09.mods.gravelminer.client.GravelMinerClient;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(GravelMiner.MOD_ID)
 public class ForgeGravelMiner {
-    public ForgeGravelMiner() {
-        Balm.initialize(GravelMiner.MOD_ID, EmptyLoadContext.INSTANCE, GravelMiner::initialize);
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(GravelMiner.MOD_ID, EmptyLoadContext.INSTANCE, GravelMinerClient::initialize));
+    public ForgeGravelMiner(FMLJavaModLoadingContext context) {
+        final var loadContext = new ForgeLoadContext(context.getModEventBus());
+        Balm.initialize(GravelMiner.MOD_ID, loadContext, GravelMiner::initialize);
+        if (FMLEnvironment.dist.isClient()) {
+            BalmClient.initialize(GravelMiner.MOD_ID, loadContext, GravelMinerClient::initialize);
+        }
     }
 
 }
