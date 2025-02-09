04b4021bd09e5521c1eb2fbb60ba8b76e23defa2
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-09 10:13:43 +0100

    fix: Enable server-only https://github.com/TwelveIterationMods/Balm/issues/122

--- common/src/main/java/net/blay09/mods/gravelminer/network/ModNetworking.java
+++ common/src/main/java/net/blay09/mods/gravelminer/network/ModNetworking.java
@@ -1,10 +1,13 @@
 package net.blay09.mods.gravelminer.network;
 
 import net.blay09.mods.balm.api.network.BalmNetworking;
+import net.blay09.mods.gravelminer.GravelMiner;
 
 public class ModNetworking {
 
     public static void initialize(BalmNetworking networking) {
+        networking.allowServerOnly(GravelMiner.MOD_ID);
+
         networking.registerClientboundPacket(HelloMessage.TYPE, HelloMessage.class, HelloMessage::encode, HelloMessage::decode, HelloMessage::handle);
         networking.registerServerboundPacket(SetClientSettingMessage.TYPE, SetClientSettingMessage.class, SetClientSettingMessage::encode, SetClientSettingMessage::decode, SetClientSettingMessage::handle);
     }
